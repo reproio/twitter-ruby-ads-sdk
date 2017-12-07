f9066a34927f1a60e0410c865a08473ee7195c49
Eiji Sakai <33144111+esakai@users.noreply.github.com>
2017-12-07 14:29:56 +0900

    Temporary workaround for twitterdev issue #107
    https://github.com/twitterdev/twitter-ruby-ads-sdk/issues/107
    
    This patch of code has been incoprated from
    kamabayashia/twitter-ruby-ads-sdk.

--- lib/twitter-ads/campaign/line_item.rb
+++ lib/twitter-ads/campaign/line_item.rb
@@ -97,5 +97,20 @@ module TwitterAds
       id ? TargetingCriteria.load(account, id, opts) : TargetingCriteria.all(account, @id, opts)
     end
 
+    def to_params
+      params = super
+
+      # If bid_type is set, automatically_select_bid must not be set.
+      params.delete(:automatically_select_bid) if params.key?(:bid_type)
+
+      # If set to true, bid_amount_local_micro must be NULL
+      params.store(:bid_amount_local_micro, nil) if params[:bid_type].to_s == Enum::BidType::AUTO.to_s && !self.id.nil?
+
+      # advertiser_user_id is currently beta-only and causes an error when sent.
+      params.delete(:advertiser_user_id)
+
+      params
+    end
+
   end
 end
