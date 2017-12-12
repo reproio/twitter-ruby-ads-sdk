bb5deabd1a8d0c62f881c99a8e5ee0c125abf15a
John Babich <jbabich@twitter.com>
2017-12-12 16:32:04 +0900

    status->text, video website card/scheduled tweets, card_uri changes (#166)
    * 1. Add Video Website Card and Scheduled Tweets, 2. Add card_uri, missing creative resource attributes, Fix test issues, 3. Fix examples
    
    * Update per code review
    
    * Fix rubocop issue

--- lib/twitter-ads/campaign/tweet.rb
+++ lib/twitter-ads/campaign/tweet.rb
@@ -31,7 +31,7 @@ module TwitterAds
       # @param opts [Hash] A hash of options.
       #
       # @option opts [Integer] :id The ID of an existing Tweet you want to preview.
-      # @option opts [String] :text The text of your text update, up to 140 characters.
+      # @option opts [String] :text The text of your status update.
       # @option opts [Array] :media_ids A list of up to four media IDs to associate with the Tweet.
       # @option opts [String] :card_id The base-36 ID of a revenue card to be embedded in the Tweet.
       # @option opts [String] :preview_target The target to render the Tweet preview
@@ -59,9 +59,9 @@ module TwitterAds
 
       # Creates a "Promoted-Only" Tweet using the specialized Ads API end point.
       #
-      # @param text [String] The main Tweet body (max: 140 characters).
       # @param opts [Hash] A hash of options.
       #
+      # @option opts [String] :text The main Tweet body.
       # @option opts [Array] :media_ids A list of up to four media IDs to associate with the Tweet.
       # @option opts [Integer] :as_user_id The user ID whom you are posting the Tweet on behalf of.
       # @option opts [Boolean] :trim_user Excludes the user object from the hydrated Tweet response.
@@ -76,10 +76,9 @@ module TwitterAds
       # @see https://dev.twitter.com/ads/reference/post/accounts/%3Aaccount_id/tweet
       #
       # @return [Hash] A hash containing the newly created Tweet object.
-      def create(account, text, opts = {})
-        params   = { text: text }.merge!(opts)
+      def create(account, opts = {})
         resource = RESOURCE_CREATE % { account_id: account.id }
-        response = TwitterAds::Request.new(account.client, :post, resource, params: params).perform
+        response = TwitterAds::Request.new(account.client, :post, resource, params: opts).perform
         response.body[:data]
       end
 
