57ca37806f6494762b875eb2d26091f78aac84ef
Eiji Sakai <33144111+esakai@users.noreply.github.com>
2018-05-14 12:56:55 +0900

    Rename user_ids to user_id in Account#timeline

--- lib/twitter-ads/account.rb
+++ lib/twitter-ads/account.rb
@@ -232,7 +232,7 @@ module TwitterAds
     # @since 0.2.3
     def scoped_timeline(ids, opts = {})
       ids      = ids.join(',') if ids.is_a?(Array)
-      params   = { user_ids: ids }.merge!(opts)
+      params   = { user_id: ids }.merge!(opts)
       resource = SCOPED_TIMELINE % { id: @id }
       request  = Request.new(client, :get, resource, params: params)
       response = request.perform
